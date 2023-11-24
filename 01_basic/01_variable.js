aaf4bb954378ae81968545aeeb7b5584daff36be
Ankit <av542939@gmail.com>
2023-11-24 11:03:07 +0530

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 123334
+var accountEmail = "av542939@gmail.com"
+let accountPass = 12234
+accountState = "Jaipur"
+let account
+
+console.table([accountId, accountEmail, accountPass, accountState, account]);
