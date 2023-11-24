831d9448467c1e0e2dfc595f6a4102f269dd29f8
Ankit <av542939@gmail.com>
2023-11-24 11:20:48 +0530

    dataType

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined=>
+// symbol=>
+
+//object
