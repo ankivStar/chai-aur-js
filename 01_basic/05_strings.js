3f606472702150ee00d1bf6faaf8a0c069317264
Ankit <av542939@gmail.com>
2023-11-27 20:04:05 +0530

    strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,10 @@
+const name = "Ankit"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Ankit');
+console.log(gameName);
+console.log(gameName.__proto__);
